748b7e984756d92d07ecae40fa480860f26a32cf
SERGIO-LAPTOP\Sarma <sarmat.p8@gmail.com>
2022-07-20 13:40:34 +0300

    Add burger menu

--- script.js
+++ script.js
@@ -59,3 +59,26 @@ prevSlide.addEventListener("click", function () {
     slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
   });
 });
+
+
+
+const menu = document.querySelector(".menu__burger_list");
+const menuItems = document.querySelectorAll(".menuItem");
+const hamburger= document.querySelector(".header__burgerMenu");
+
+
+function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+  } else {
+    menu.classList.add("showMenu");
+  }
+}
+
+hamburger.addEventListener("click", toggleMenu);
+
+menuItems.forEach( 
+  function(menuItem) { 
+    menuItem.addEventListener("click", toggleMenu);
+  }
+)
